fdfe6012ecb41faf2fb00dd14ab1f59d964b8a13
EHansonn <evanhanson02@gmail.com>
2023-03-06 04:27:59 -0500

    navbar tweaks

--- components/header.tsx
+++ components/header.tsx
@@ -112,32 +112,37 @@ export default function Header() {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Your Profile #TODO
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Settings #TODO
-                          </a>
-                        )}
-                      </Menu.Item>
+                      {session && (
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Your Profile #TODO
+                            </a>
+                          )}
+                        </Menu.Item>
+                      )}
+                      {session && (
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Settings #TODO
+                            </a>
+                          )}
+                        </Menu.Item>
+                      )}
+
                       <Menu.Item>
                         {({ active }) => (
                           <Link
