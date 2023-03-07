b133a63a503f8f0709ce106925f4156e6370a0ab
EHansonn <evanhanson02@gmail.com>
2023-03-07 01:30:05 -0500

    test

--- pages/api/example.ts
+++ pages/api/example.ts
@@ -1,14 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(req:NextApiRequest, res:NextApiResponse) {
+import prisma from "../../lib/prisma";
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
 
   const { APP_KEY } = process.env;
   
   const  ACTION_KEY  = req?.headers?.authorization
-
+  console.log(ACTION_KEY)
   try {
     if (ACTION_KEY === APP_KEY) {
       // Process the POST request
+      console.log("made it in equals")
+      const post =await prisma.questsOnUser.deleteMany({
+        where: { quest: {
+          repeatable: "daily"
+        } },
+      });
+      console.log(post)
       res.status(200).json({ success: 'true' })
     } else {
       res.status(401)
