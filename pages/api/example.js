b79d4320cec1bb7f89d15827ca54309220b1f243
EHansonn <evanhanson02@gmail.com>
2023-03-07 00:37:37 -0500

    cron test

--- pages/api/example.js
+++ pages/api/example.js
@@ -1,10 +1,19 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { APP_KEY } = process.env;
   const { ACTION_KEY } = req.headers.authorization.split(" ")[1];
 
   try {
     if (ACTION_KEY === APP_KEY) {
       // Process the POST request
+      const deleteDaily = await prisma.questsOnUser.deleteMany({
+        where: {
+          quest: {
+            repeatable: {contains: 'daily',}
+            
+          },
+        },
+      })
+
       console.log("Happeneeddd1231231023921312");
       res.status(200).json({ success: "true" });
     } else {
