599d565893d226855b2ad95a4bc02d90eb01d8e7
EHansonn <evanhanson02@gmail.com>
2023-03-07 13:28:29 -0500

    cleaned up code w/ prettier

--- pages/api/dailyreset.ts
+++ pages/api/dailyreset.ts
@@ -1,28 +1,32 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../lib/prisma";
-export default async function handler(req:NextApiRequest, res:NextApiResponse) {
-
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { APP_KEY } = process.env;
-  
-  const  ACTION_KEY  = req?.headers?.authorization
+
+  const ACTION_KEY = req?.headers?.authorization;
   //console.log(ACTION_KEY)
   try {
     if (ACTION_KEY === APP_KEY) {
       // Process the POST request
-      console.log("made it in equals")
-      const post =await prisma.questsOnUser.deleteMany({
-        where: { quest: {
-          repeatable: "daily"
-        } },
+      console.log("made it in equals");
+      const post = await prisma.questsOnUser.deleteMany({
+        where: {
+          quest: {
+            repeatable: "daily",
+          },
+        },
       });
-      console.log(post)
-      res.status(200).json({ success: 'true' })
+      console.log(post);
+      res.status(200).json({ success: "true" });
     } else {
-      res.status(401)
-      res.end()
+      res.status(401);
+      res.end();
     }
-  } catch(err) {
-    res.status(500)
-    res.end()
+  } catch (err) {
+    res.status(500);
+    res.end();
   }
 }
