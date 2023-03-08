cab057bc145660522b29e141a50b78ff2abe6105
EHansonn <evanhanson02@gmail.com>
2023-03-07 22:52:41 -0500

    updated antd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
 };
 
-module.exports = nextConfig;
+(module.exports = {
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+}),
+  nextConfig;
