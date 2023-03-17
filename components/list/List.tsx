feacbd4f538748465d41d132405812ce0e573ca8
EHansonn <evanhanson02@gmail.com>
2023-03-16 22:48:06 -0400

    Added drop down menu for lists.

--- components/list/List.tsx
+++ components/list/List.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Quest, User } from "../../pages/yourdailies";
 import Router from "next/router";
 import { Button } from "antd";
 import ListQuestAndRow from "./ListQuestsandmodal";
-import { CloseOutlined } from "@ant-design/icons";
+import { CloseOutlined, DownOutlined, UpOutlined } from "@ant-design/icons";
 export type ListProps = {
   tasks: Quest[] | undefined;
   id: string;
@@ -30,10 +30,23 @@ export type QuestProps = {
 };
 
 const List: React.FC<{ list: ListProps; user: User }> = ({ list, user }) => {
+  const [active, setActive] = useState(false);
+
+  const showDetails = () => {
+    setActive((currVal) => {
+      return !currVal;
+    });
+  };
+
   return (
     <div className={`flex flex-col object-contain text-offwhite-50  `}>
-      <div className="flex justify-between">
-        <h3 className="mt-0  pt-0 mb-0 inline-block align-baseline">{list.title} </h3>
+      <div
+        className="flex justify-between cursor-pointer hover:bg-slate-600 "
+        onClick={showDetails}
+      >
+        <h3 className="mt-0  pt-0 mb-0 inline-block align-baseline">
+          {list.title}{" "}
+        </h3>
         {/* <Bu pt-0 tton
           
           onClick={async () => {
@@ -45,22 +58,42 @@ const List: React.FC<{ list: ListProps; user: User }> = ({ list, user }) => {
         >
           Delete
         </Button> */}
-        <CloseOutlined className="mt-1 hover:bg-slate-600 cursor-pointer h-[16px] w-[16px]" onClick={async () => {
-            await fetch(`/api/list/${list.id}`, {
-              method: "DELETE",
-            });
-            Router.push("/yourdailies");
-          }} style={{color: "white"}}></CloseOutlined>
+        <div>
+          <DownOutlined
+            className={`mt-1 mr-5 hover:bg-slate-600 cursor-pointer h-[16px] w-[16px] ${
+              active ? "hidden" : ""
+            }`}
+          />
+          <UpOutlined
+            className={`mt-1 mr-5 hover:bg-slate-600 cursor-pointer h-[16px] w-[16px] ${
+              active ? "" : "hidden"
+            }`}
+          />
+          <CloseOutlined
+            className="mt-1 hover:bg-slate-600 cursor-pointer h-[16px] w-[16px]"
+            onClick={async () => {
+              await fetch(`/api/list/${list.id}`, {
+                method: "DELETE",
+              });
+              Router.push("/yourdailies");
+            }}
+            style={{ color: "white" }}
+          ></CloseOutlined>
+        </div>
       </div>
-
-      <h4 className="mt-0 mb-2">{list.content}</h4>
-      <h4 className="mt-0 mb-0">Selected Quests:</h4>
-      <div className="flex flex-col ">
-        {list?.tasks!.map((quest: Quest) => {
-          return (
-            <ListQuestAndRow key={quest.value} quest={quest}></ListQuestAndRow>
-          );
-        })}
+      <div className={`${active ? "" : "hidden"}`}>
+        <h4 className="mt-0 mb-2">{list.content}</h4>
+        <h4 className="mt-0 mb-0">Selected Quests:</h4>
+        <div className="flex flex-col ">
+          {list?.tasks!.map((quest: Quest) => {
+            return (
+              <ListQuestAndRow
+                key={quest.value}
+                quest={quest}
+              ></ListQuestAndRow>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
