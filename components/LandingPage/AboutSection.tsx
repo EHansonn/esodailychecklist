613cf7b95cfaae41d71054079c880206b54cb07a
EHansonn <evanhanson02@gmail.com>
2023-03-22 17:51:51 -0400

    Changed width to match the features section

--- components/LandingPage/AboutSection.tsx
+++ components/LandingPage/AboutSection.tsx
@@ -1,30 +1,39 @@
 import {
-    BulbOutlined,
-    GithubOutlined,
-    LoadingOutlined,
-    TeamOutlined,
-    UnorderedListOutlined,
-  } from "@ant-design/icons";
-  import { Button, Spin } from "antd";
-  import { signIn, useSession } from "next-auth/react";
-  import Head from "next/head";
-  import Image from "next/image";
-  import Link from "next/link";
+  BulbOutlined,
+  GithubOutlined,
+  LoadingOutlined,
+  TeamOutlined,
+  UnorderedListOutlined,
+} from "@ant-design/icons";
+import { Button, Spin } from "antd";
+import { signIn, useSession } from "next-auth/react";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import { MutableRefObject, useRef } from "react";
-  import Header from "../header";
-  import Layout from "../layout";
-  import styles from "./index.module.css";
- 
-const AboutSection : React.FC<{innerRef:MutableRefObject<null | HTMLDivElement> }>= ({innerRef}) => {
+import Header from "../header";
+import Layout from "../layout";
+import styles from "./index.module.css";
 
-    return <div  ref={innerRef} className=" pt-20 pb-20"><div className="relative pb-2   m-auto  rounded-lg backdrop-blur-md  bg-slate-800 brightness-90 text-offwhite-50 w-1/2 text-center">
-    <h1 className="mt-0 mb-2 pb-0 pt-2">About Us</h1>
-    <div className="flex flex-col sm:flex-row ">
-    <p className="text-xl text-center text-white  max-w-6xl m-auto">
-      ESO Daily Checklist is a small ToDo app created by a former ESO player who understands how hard keeping track of your dailies can be. While in-game addons can provide similar functionality, the online, cross-platoform nature of a web app allows you to more easily manage your dailies across multiple devices.
-    </p>
+const AboutSection: React.FC<{
+  innerRef: MutableRefObject<null | HTMLDivElement>;
+}> = ({ innerRef }) => {
+  return (
+    <div ref={innerRef} className=" pt-20 pb-20">
+      <div className="relative  m-auto  pt-3 pb-4 px-3 rounded-lg backdrop-blur-md  bg-slate-800 brightness-90 text-offwhite-50 w-1/2 text-center">
+        <h1 className="mt-0 mb-2 pb-0 pt-2">About Us</h1>
+        <div className="flex flex-col sm:flex-row ">
+          <p className="text-xl text-center text-white  max-w-6xl m-auto">
+            ESO Daily Checklist is a small ToDo app created by a former ESO
+            player who understands how hard keeping track of your dailies can
+            be. While in-game addons can provide similar functionality, the
+            online, cross-platoform nature of a web app allows you to more
+            easily manage your dailies across multiple devices.
+          </p>
+        </div>
+      </div>
     </div>
-  </div></div>
-}
+  );
+};
 
-export default AboutSection
+export default AboutSection;
