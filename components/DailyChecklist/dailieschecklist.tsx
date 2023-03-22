33a5c9fb912f0226e08d9c6274cc3253e2f4ff3a
EHansonn <evanhanson02@gmail.com>
2023-03-22 13:23:03 -0400

    Removed ability to select radio text

--- components/DailyChecklist/dailieschecklist.tsx
+++ components/DailyChecklist/dailieschecklist.tsx
@@ -284,7 +284,7 @@ const YourDailiesChecklist: NextPage<Props> = ({ user, lists, quests }) => {
               </div>
 
               <div className="flex flex-col space-y-3 lg:w-1/3 md:w-1/3 sm:w-full lg:mt-0 md:mt-0 mt-14  ">
-                <div className="w-full flex justify-center absolute -top-32  md:relative md:top-0 ">
+                <div className="w-full flex justify-center absolute -top-32  md:relative md:top-0 select-none ">
                   <Radio.Group
                     options={[
                       { value: "All Quests", label: "All Quests" },
