8e34d642bee4de1810c08adc5d581118b51fc1e5
EHansonn <evanhanson02@gmail.com>
2023-03-24 00:05:56 -0400

    Cleaned up unused imports

--- pages/api/quest/index.ts
+++ pages/api/quest/index.ts
@@ -1,25 +1,7 @@
-// pages/api/post/index.ts
-
-import { getServerSession } from "next-auth/next";
 import prisma from "../../../lib/prisma";
-import { authOptions } from "../auth/[...nextauth]";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { User } from "../../../components/DailyChecklist/dailiesCheckList";
 
-export interface ListBody {
-	title: string;
-	content: string;
-	tasks: string[];
-}
-
-interface UserApiRequest extends NextApiRequest {
-	body: ListBody;
-	query: {
-		id: string;
-	};
-}
-
-export default async function handle(req: UserApiRequest, res: NextApiResponse) {
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
 	//If we eventually get enough concurrent users, ill switch over to static props, but on the free vercel tier its not worth it when you have a small userbase imo.
 	try {
 		const availableQuests = await prisma?.quest.findMany({});
