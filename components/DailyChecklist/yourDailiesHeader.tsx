eeb2f2d94a9b72c6e06bfce4e2d7aba5ede89bac
EHansonn <evanhanson02@gmail.com>
2023-03-23 22:42:10 -0400

    Added favicon to head

--- components/DailyChecklist/yourDailiesHeader.tsx
+++ components/DailyChecklist/yourDailiesHeader.tsx
@@ -9,6 +9,7 @@ const YourDailiesHeader = () => {
 				content="Keep track of the 100+ repeatable quests in the Elder Scrolls Online. Simply login with your google account, create one or more characters, and visit your daily checklist. There you can see every single possible repeatable task and quest in the game. You can check off the ones you've done. Come back tomorrow and you'll find that all your dailies have been reset, so you can get started right away on your tasks!"
 			/>
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<link rel="icon" href="/favicon.ico"></link>
 		</Head>
 	);
 };
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -31,6 +31,7 @@ export default function Dailies() {
 					<title>Please Sign In</title>
 					<meta name="ESO Daily Checklist - ESO ToDO List" content="" />
 					<meta name="viewport" content="width=device-width, initial-scale=1" />
+					<link rel="icon" href="/favicon.ico"></link>
 				</Head>
 				{status === "loading" && <LoadingSpinnerComponent text={"user"} />}
 				{status === "unauthenticated" && <SignInComponent text={"profile"} />}
