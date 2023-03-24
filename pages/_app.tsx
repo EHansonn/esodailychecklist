0f8e0c405353cb8c8c6e3242df1256d54b581d09
EHansonn <evanhanson02@gmail.com>
2023-03-23 22:40:49 -0400

    Added favicon to head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,6 +31,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
 					content="Keep track of the 100+ repeatable quests in the Elder Scrolls Online. Simply login with your google account, create one or more characters, and visit your daily checklist. There you can see every single possible repeatable task and quest in the game. You can check off the ones you've done. Come back tomorrow and you'll find that all your dailies have been reset, so you can get started right away on your tasks!"
 				/>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico"></link>
 			</Head>
 			<Component {...pageProps} />
 		</SessionProvider>
