813aefc650b7f79febce15b8478456d135a12856
EHansonn <evanhanson02@gmail.com>
2023-06-17 18:32:20 -0400

    padding issue

--- components/DailyChecklist/dailiesCheckList.tsx
+++ components/DailyChecklist/dailiesCheckList.tsx
@@ -205,7 +205,7 @@ const YourDailiesChecklist: NextPage<Props> = ({ user, lists, quests, categories
 							<div className="text-slate-300 pb-2 text-center ">
 								{`Daily quests reset at ${time} each day`}
 							</div>
-							<div className="text-slate-300 pl-0  lg:pl-5 md:pl-5 text-center pb-6 md:pb-0 mb-8 md:mb-0 ">
+							<div className="text-slate-300 pl-0  lg:pl-5 md:pl-5 text-center pb-6 md:pb-0 ">
 								{`Weekly quests reset at ${time} on monday`}
 							</div>
 						</div>
@@ -233,7 +233,7 @@ const YourDailiesChecklist: NextPage<Props> = ({ user, lists, quests, categories
 							</div>
 
 							<div className="flex flex-col space-y-3 lg:w-1/3 md:w-1/3 sm:w-full lg:mt-0 md:mt-0 mt-14  ">
-								<div className="w-full flex justify-center -top-32  md:relative md:top-0 select-none md:relative absolute md:top-0 -top-10">
+								<div className="w-full flex justify-center   md:relative md:top-0 select-none md:relative absolute md:top-0 -top-6 ">
 									{/* Filter Selector */}
 									<Radio.Group
 										options={[
