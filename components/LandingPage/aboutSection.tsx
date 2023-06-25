b04f9fdf48a746e7170f4788d9b137bb4015e943
EHansonn <evanhanson02@gmail.com>
2023-06-25 13:23:46 -0400

    feat: updated margin on mobile

--- components/LandingPage/aboutSection.tsx
+++ components/LandingPage/aboutSection.tsx
@@ -8,7 +8,7 @@ const AboutSection: React.FC<{
 			<div className="relative  m-auto  pt-1 pb-4 px-3 rounded-lg backdrop-blur-md   text-offwhite-50 w-9/10 md:w-1/2 text-center">
 				<h1 className="mt-0 mb-2 pb-0 pt-2">About Us</h1>
 				<div className="flex flex-col sm:flex-row ">
-					<p className="text-xl text-center text-white  max-w-6xl m-auto">
+					<p className="text-xl text-center text-white  max-w-6xl m-auto text-left">
 						ESO Daily Checklist is a small ToDo app created by a former ESO player who understands how hard
 						keeping track of your dailies can be. While in-game addons can provide similar functionality,
 						the online, cross-platoform nature of a web app allows you to more easily manage your dailies
--- components/LandingPage/logoSection.tsx
+++ components/LandingPage/logoSection.tsx
@@ -7,11 +7,11 @@ const LogoSection: React.FC<{ scrollToFunction: Function }> = ({ scrollToFunctio
 	const { data: session, status } = useSession();
 
 	return (
-		<div className="  pt-3 px-3  backdrop-blur-md mb-20 mt-20   border-b-2 border-solid border-l-0 border-r-0 border-t-0">
+		<div className="  pt-3 px-3  backdrop-blur-md mb-20 mt-0 md:mt-20   border-b-2 border-solid border-l-0 border-r-0 border-t-0">
 			<div className="text-6xl text-white text-center">
 				<Image src="/logo3.png" layout="intrinsic" height={200} width={800} alt="logo"></Image>
 			</div>
-			<p className="text-2xl text-center text-white pt-4 max-w-6xl m-auto">
+			<p className=" md:text-2xl text-center text-white pt-4 max-w-6xl m-auto">
 				A simple and easy way for you to manage and keep track of the 100+ repeatable quests in the game. Simply
 				login with your google account, create one or more characters, and visit your daily checklist. There you
 				can see every single possible repeatable task and quest in the game. You can check off the ones you've
