2c2a3be8ea72b80261c9e09b9be7c9d1a69420d6
EHansonn <evanhanson02@gmail.com>
2024-01-04 23:52:47 -0500

    feat: added necrom support

--- pages/yourdailies.tsx
+++ pages/yourdailies.tsx
@@ -72,6 +72,7 @@ export default function Dailies() {
 		"Deadlands Quests",
 		"High Isle Quests",
 		"Galen Quests",
+		"Necrom Quests",
 		"Cyrodiil Settlement Quests",
 		"Miscellaneous",
 	];
@@ -97,6 +98,7 @@ export default function Dailies() {
 		"Deadlands Quests",
 		"High Isle Quests",
 		"Galen Quests",
+		"Necrom Quests",
 	];
 
 	if (!session) {
